0583959e521051d1817ccddad58c33bdd6d566f3
shubhamaug02 <02augshubhampatel@gmail.com>
2025-11-22 17:56:57 +0530

    news feed project setup

---
+++ frontend/news-feed-ui/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        News Feed
+        <Button>Count</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
