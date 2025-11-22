00bc73ea71fd33e1c133001e19adcdc5a8adf1f9
shubhamaug02 <02augshubhampatel@gmail.com>
2025-11-22 22:54:20 +0530

    DB connect setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose
+ .connect(process.env.MONGODB_URL)
+ .then(() =>{
+   console.log("database connected")
+}).catch((err) => {
+  console.log(err)
+}); 
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server started at port 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server started at port 5000");
 });
