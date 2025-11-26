9c7fbc3725b6a3357ec548b469f077e4130d009b
shubhamaug02 <02augshubhampatel@gmail.com>
2025-11-27 02:55:32 +0530

    Added React router dom

--- frontend/news-feed-ui/src/App.jsx
+++ frontend/news-feed-ui/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import { Button } from './components/ui/button'
+import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import NewsArticle from './pages/NewsArticle'
+import SignInForm from './auth/forms/SignInForm'
+import SignUpForm from './auth/forms/SignUpForm'
 
 function App() {
   return (
-    <>
-      <div className='text-3xl font-bold underline'>
-        News Feed
-        <Button>Count</Button>
-      </div>
-    </>
+      <BrowserRouter>
+         <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/about" element={<About />} />
+           <Route path="/dashboard" element={<Dashboard />} />
+           <Route path="/news" element={<NewsArticle />}/>
+
+           <Route path="/sign-in" element={<SignInForm />} />
+           <Route path="/sign-up" element={<SignUpForm />} />
+         </Routes>
+      </BrowserRouter>
   )
 }
 
